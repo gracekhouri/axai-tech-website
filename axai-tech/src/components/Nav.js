543bfc2342e1ae8f5b687e3b2fbefb2863cfe934
Julia Moody <juliamoody@Julias-MacBook-Pro.local>
2021-06-11 11:06:34 -0500

    product pg

--- axai-tech/src/components/Nav.js
+++ axai-tech/src/components/Nav.js
@@ -7,7 +7,7 @@ export default class Nav extends Component {
         return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-              <Link to="/"><img src={ximage}></img></Link>
+              <Link to="/"><img src={ximage} alt=""></img></Link>
                 {/* <a className="navbar-brand" href="#"><img src={ximage}></img></a> */}
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
