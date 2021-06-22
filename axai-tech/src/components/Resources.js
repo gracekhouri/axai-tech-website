3bb62e0e7c284b5e32b93c417c026b48bca0f134
dark1kn1ght <77123546+dark1kn1ght@users.noreply.github.com>
2021-06-22 13:01:33 -0400

    Update Resources.js

--- axai-tech/src/components/Resources.js
+++ axai-tech/src/components/Resources.js
@@ -13,15 +13,17 @@ export default class Resources extends Component {
                        Download and complete the fill-able .pdf form below, 
                        and submit to clinical@axaitech.co.za.
                     </p> 
-                    <i className="bi bi-file-earmark-pdf-fill pdf-change m-5"></i>
+                    <a target="_blank" rel="noopener noreferrer" href="https://firebasestorage.googleapis.com/v0/b/axai-tech.appspot.com/o/Axaitech_Med_v2.pdf?alt=media&token=f76c4363-2bbc-4345-816e-efbb724f5056">
+                        <i className="bi bi-file-earmark-pdf-fill pdf-change m-5"></i>
+                    </a>
                 </div>
                 <div className="p-3">
                     <h1 className="">Information and Sample Report</h1>
                 </div>
                 <div className="">
-                    <i className="bi bi-file-earmark-pdf-fill pdf-change m-5"></i>
-                    <i className="bi bi-file-earmark-pdf-fill pdf-change m-5"></i>
-                    <i className="bi bi-file-earmark-pdf-fill pdf-change m-5"></i>
+                    <a target="_blank" rel="noopener noreferrer" href="https://firebasestorage.googleapis.com/v0/b/axai-tech.appspot.com/o/Sample-clinical-report_Creatori(1)(1).pdf?alt=media&token=477eddbb-6c58-44a4-9e00-5f9a2fbcdba4">
+                        <i className="bi bi-file-earmark-pdf-fill pdf-change m-5"></i>
+                    </a>  
                 </div>
                 <div className="p-5"></div>
             </div>
